336d688146938dbbef719aacabfa45464932cd1f
Chris Barr <chris@chrisbarrbuilds.com>
2022-06-03 20:53:31 +0930

    Initial structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# TAS5711 Arduino Library
+This is an Arduino library for the TAS5711 I2S DAC. This library provides methods to control the TAS5711 via I2C. It is not an audio library - refer to other libraries for audio.I2S functionality.
+
+## Key Features
+
+## Examples
+Refer to the examples folder for more detailed examples on usage. 
---
+++ src/TAS5711.h
@@ -0,0 +1,18 @@
+/*************************************************** 
+ This is a library for the TAS5711 I2S DAC/AMP.
+
+ Written by Chris Barr, 2022.
+ ****************************************************/
+
+#ifndef _TAS5711_H_
+#define _TAS5711_H_
+
+#include <stdint.h>
+#include <Arduino.h>
+
+namespace TAS5711 {
+
+
+} // namespace _TAS5711_H_
+
+#endif
